00546e44e60b93c663cab07f83e9828c2c154cc7
Abhishek Das <abhishekdas@Abhisheks-MacBook-Air.local>
2020-10-01 23:49:41 -0400

    fifth commit

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement} from 'lwc';
     export default class HelloWorld extends LightningElement {
-        greeting = 'XYZ';
+        greeting = 'XY';
         changeHandler(event) {
             this.greeting = event.target.value;
         }
